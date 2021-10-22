b06747233d80122837f3c508271cfc0c06a48f9a
ab-rai <you@netheadab@gmail.com>
2021-10-23 00:15:12 +0530

    Removing spring boot

--- src/main/java/com/in28minutes/spring/basics/abspringin5steps/AbSpringIn5StepsBasicApplication.java
+++ src/main/java/com/in28minutes/spring/basics/abspringin5steps/AbSpringIn5StepsBasicApplication.java
@@ -1,17 +1,20 @@
 package com.in28minutes.spring.basics.abspringin5steps;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.spring.basics.abspringin5steps.basic.BinarySearchImpl;
 
 // this annotation make spring automatically scan current packages and sub packages which it is in
-@SpringBootApplication
+@Configuration
+@ComponentScan("com.in28minutes.spring.basics.abspringin5steps")
 public class AbSpringIn5StepsBasicApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext appContext= SpringApplication.run(AbSpringIn5StepsBasicApplication.class, args);
+		ConfigurableApplicationContext appContext=new AnnotationConfigApplicationContext(AbSpringIn5StepsBasicApplication.class);
 		BinarySearchImpl binarySearch =appContext.getBean(BinarySearchImpl.class);
 		BinarySearchImpl binarySearch2 =appContext.getBean(BinarySearchImpl.class);
 		System.out.println(binarySearch);
